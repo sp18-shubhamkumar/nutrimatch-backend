790dec382397f7e67c2cc93d0aeb3a1a88a5fa38
sp18-shubhamkumar <shubham.kumar@sparkeighteen.com>
2025-07-23 11:25:06 +0530

    new apis (food item management)

--- nutrimatch/api/views/food_management.py
+++ nutrimatch/api/views/food_management.py
@@ -1,11 +1,81 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from ..permissions import IsRestaurantAdminOrEmployee
-from ..serializers import FoodItemSerializer
+from ..serializers import FoodItemSerializer 
+from ..models import Restaurant, Employee, FoodItem
+from rest_framework.response import Response
+from drf_yasg.utils import swagger_auto_schema
 
 
 class FoodItemView(APIView):
-    permission_classes = [IsAuthenticated, IsRestaurantAdminOrEmployee]
-
+    permission_classes = [IsAuthenticated, IsRestaurantAdminOrEmployee]\
+    
+    
+    def get_restaurant(self, rid, user):
+        try:
+            restaurant = Restaurant.objects.get(id=rid)
+            if restaurant.owner == user or Employee.objects.filter(user=user, restaurant=restaurant).exists():
+                return restaurant
+        except Restaurant.DoesNotExist:
+                return None
+        
+    @swagger_auto_schema(request_body=FoodItemSerializer)
     def post(self, rid, request):
-        pass
+        restaurant = self.get_restaurant(rid, request.user)
+        if not restaurant:
+             return Response({'error':'Unauthorized or restaurant not found'}, status=403)
+        
+        serializer = FoodItemSerializer(data=request.data, context={'restaurant':restaurant})
+
+        if serializer.is_valid():
+            serializer.save(restaurant=restaurant)
+            return Response({'message':'Food Item added Successfully', 'data': serializer.data}, status=201)
+        return Response(serializer.errors, status=400)
+    
+    def get(self, request, rid, fid=None):
+        restaurant = self.get_restaurant(rid, request.user)
+        if not restaurant:
+             return Response({'error':'Unauthorized or restaurant not found'}, status=403)
+        
+        if fid:
+            try:
+                food = FoodItem.objects.get(id=fid, restaurant=restaurant)
+                return Response(FoodItemSerializer(food).data, context={'restaurant':restaurant})
+            except FoodItem.DoesNotExist:
+                return Response({'error':'Food Item not found'}, status=404)
+             
+        foods = FoodItem.objects.filter(restaurant=restaurant)
+        return Response(FoodItemSerializer(foods, many=True, context={'restaurant':restaurant}).data)
+    
+
+    @swagger_auto_schema(request_body=FoodItemSerializer)
+    def patch(self, request, rid, fid):
+        restaurant = self.get_restaurant(rid, request.user)
+        if not restaurant:
+            return Response({'error':'Unauthorized or restaurant not found'}, status=403)
+         
+        try:
+            food = FoodItem.objects.get(id=fid, restaurant=restaurant)
+        except FoodItem.DoesNotExist:
+            return Response({'error': 'Food Item not found'}, status=404)
+        serializer = FoodItemSerializer(food, data=request.data, partial=True, context={'restaurant':restaurant})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Food item updated', 'data':serializer.data})
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, rid, fid):
+        restaurant = self.get_restaurant(rid, request.user)
+        if not restaurant:
+            return Response({'error':'Unauthorized or restaurant not found'}, status=403)
+
+        try:
+            food = FoodItem.objects.get(id=fid, restaurant=restaurant)
+            food.delete()
+            return Response({'message':'Food Item is Deleted'}, status=204)
+        except FoodItem.DoesNotExist:
+            return Response({'error':'Food Item not Found'}, status=404)
+         
+            
+              
+             
--- nutrimatch/api/views/urls.py
+++ nutrimatch/api/views/urls.py
@@ -1,19 +1,24 @@
 from django.urls import path
-from . import RegisterView, LoginView, RestaurantsOperationView, EmployeeManagementView
+from . import RegisterView, LoginView, RestaurantsOperationView, EmployeeManagementView, FoodItemView
 
 
 urlpatterns = [
 
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+     path('register/', RegisterView.as_view(), name='register'),
+     path('login/', LoginView.as_view(), name='login'),
 
-    path('restaurants/', RestaurantsOperationView.as_view(), name='restaurants'),
-    path('restaurants/<int:rid>/',
-         RestaurantsOperationView.as_view(), name='restaurants'),
+     path('restaurants/', RestaurantsOperationView.as_view(), name='restaurants'),
+     path('restaurants/<int:rid>/',
+          RestaurantsOperationView.as_view(), name='restaurants'),
 
-    path('restaurants/<int:rid>/employees/',
-         EmployeeManagementView.as_view(), name='employees'),
-    path('restaurants/<int:rid>/employees/<int:eid>/',
-         EmployeeManagementView.as_view(), name='employees'),
+     path('restaurants/<int:rid>/employees/',
+          EmployeeManagementView.as_view(), name='employees'),
+     path('restaurants/<int:rid>/employees/<int:eid>/',
+          EmployeeManagementView.as_view(), name='employees'),
+     
+     path('restaurants/<int:rid>/food-items/',
+         FoodItemView.as_view(), name='food-items'),
+     path('restaurants/<int:rid>/food-items/<int:fid>/',
+         FoodItemView.as_view(), name='food-items'),
 
 ]
