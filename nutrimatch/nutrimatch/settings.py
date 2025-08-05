5ebdbe0c3fbac602f545df9d6f90c1797c7dcf4d
sp18-shubhamkumar <shubham.kumar@sparkeighteen.com>
2025-08-05 12:24:47 +0530

    settings.py updated

--- nutrimatch/nutrimatch/settings.py
+++ nutrimatch/nutrimatch/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.235.74.90', 'localhost']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 AUTH_USER_MODEL = 'api.User'
 
