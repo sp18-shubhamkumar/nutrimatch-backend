4c15017259c529ff232275a558236c52e29a0d42
sp18-shubhamkumar <shubham.kumar@sparkeighteen.com>
2025-08-06 12:15:09 +0530

    new tasks in tasks.py

--- nutrimatch/nutrimatch/tasks.py
+++ nutrimatch/nutrimatch/tasks.py
@@ -9,3 +9,8 @@ logger = logging.getLogger(__name__)
 def my_background_task():
     logger.info("Starting background task")
     logger.info("Finished background task")
+
+@shared_task
+def test_celery_task():
+    print("Celery is working")
+    return "task complete"
